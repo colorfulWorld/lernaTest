44cefb5a55fb78e5996d606d4c7fb962fa6bb9b3
qiujb <314710634@qq.com>
2021-11-01 10:37:06 +0800

    feat: eslintCheck 更新eslint规则，适配touch_sub_front

--- packages/zwt-fe/src/eslint-check/1/eslintrc.js
+++ packages/zwt-fe/src/eslint-check/1/eslintrc.js
@@ -4,6 +4,11 @@ module.exports = {
         'es6': true,
         'node': true
     },
+    'globals': {
+        '_': true,
+        '$': true,
+        'WadeMobile': true,
+    },
     'extends': [
         'eslint:recommended',
         // 'plugin:vue/essential'
@@ -27,6 +32,15 @@ module.exports = {
         'vue/html-indent': ['error', 4],
         'vue/max-attributes-per-line': ['error', {
             'singleline': 4
-        }]
+        }],
+        'object-curly-spacing': ['error', 'always', { 'objectsInObjects': false }], // 对象内两侧空格
+        'keyword-spacing': ['error'], // 关键字(if, else, function 等)空格，默认两端true
+        'space-before-function-paren': ['error', 'never'], // 函数名与参数之间空格fn()
+        'space-before-blocks': ['error', 'always'], // 块之前空格 fn() {}
+        'key-spacing': ['error'], // 对象冒号和value之间有空格
+        'space-infix-ops': ['error'], // 中缀操作符+-*/%前后有空格
+        'space-unary-ops': ['error'], // 一元操作符++,--,new,typeof等前后的空格
+        'comma-spacing': ['error'], // 逗号前后空格
+        'no-prototype-builtins': ['off'] // 允许使用 xxx.hasOwnProperty()方法
     }
 };
